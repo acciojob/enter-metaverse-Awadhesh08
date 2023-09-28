3dc154f3611ee6ac7254c3beaf8daff80ff446ac
Awadhesh <72164418+Awadhesh08@users.noreply.github.com>
2023-09-28 14:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ button.addEventListener("click", ()=>{
 	let h1 = document.createElement("h1");
 	h1.id = "status";
 	h1.innerText = "Entered Metaverse";
-	document.body.insertBefore(h1,para);
+	para.parentNode.replaceChild(h1, para); 
+	// document.body.insertBefore(h1,para);
 	para.remove();
 })
